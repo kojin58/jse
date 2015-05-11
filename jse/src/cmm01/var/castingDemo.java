ea5e21764ab41c4bdc1f7c4149f6dc8f4772ec57
Administrator <jongdal@gmail.com>
2015-05-11 11:12:46 +0900

    launching..

---
+++ jse/src/cmm01/var/castingDemo.java
@@ -0,0 +1,26 @@
+package cmm01.var;
+/*
+Date : 2015.05.11
+Author : 고진광
+Description : 캐스팅 문법에 대한 학습자료 
+ * 
+ * 
+ * */
+class castingDemo{
+	public static void main(String[] args) {
+/*
+Up-casting
+변환 (conversion)
+promotion
+묵시적(Implict)자동변환 
+ * 
+ * 
+ * */		
+		
+		
+		System.out.println("HelloJava");
+	
+	}
+	
+}
+
