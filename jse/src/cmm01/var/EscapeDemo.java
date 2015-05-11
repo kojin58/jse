db4f6a19ef40585743450c3b5185068cb1360a97
Administrator <jongdal@gmail.com>
2015-05-11 12:01:45 +0900

    escape study

--- jse/src/cmm01/var/EscapeDemo.java
+++ jse/src/cmm01/var/EscapeDemo.java
@@ -1,6 +1,17 @@
 package cmm01.var;
 
-
 public class EscapeDemo {
-
+    public static void main(String[] args) {
+		int kor = 90;
+		int eng = 85;
+		int sum = 0;
+		
+		sum = kor + eng;
+		
+		System.out.print("종합성적\n");
+		System.out.println("====================");
+		System.out.println("국어\영어\수학");
+	    System.out.println(kor+"\t"+ eng+"\t"+sum);
+	}
+    
 }
